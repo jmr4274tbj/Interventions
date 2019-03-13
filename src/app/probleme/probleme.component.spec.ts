b48acf813324199e233ebc17b7e3959f62533751
jmr4274tbj <fivecores22@hotmail.com>
2019-03-13 16:04:15 -0400

    Zone PRÉNOM valide avec 200 caractères

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -37,4 +37,10 @@ describe('ProblemeComponent', () => {
     expect(zone.valid).toBeTruthy();
   });
 
+  it('Zone PRÉNOM valide avec 200 caractères', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(200));
+    expect(zone.valid).toBeTruthy();
+  });
+
 });
