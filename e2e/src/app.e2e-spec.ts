6473fa0cbab804f40b6a140c307548f89e0ae578
rtar019 <rayane.taleb@hotmail.com>
2019-04-11 13:41:56 -0400

    TP17 en cours

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,16 +8,38 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('#37 | Doit afficher le titre du formulaire Déclarer un problème', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Interventions!');
+    expect(page.getTitleText()).toEqual('Déclarer un problème');
   });
 
-  afterEach(async () => {
+  it('#38 | Doit activer le bouton Sauvegarder avec champs valides scénario nominal', () => {
+    page.setChampsValidesScenarioNominal();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('#39 | Doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par message TEXTE', () => {
+    page.setChampsValidesScenarioAlternatifParMessageTexte();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('#40 | Doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par courriel', () => {
+
+  });
+
+  it('#41 | Zone DESCRIPTION DU PROBLÈME a une bordure VERTE si nombre de caractères suffisant', () => {
+
+  });
+
+  it('#42 | Zone DESCRIPTION DU PROBLÈME a une bordure ROUGE si nombre de caractères insuffisant ', () => {
+
+  });
+
+  /*afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
     expect(logs).not.toContain(jasmine.objectContaining({
       level: logging.Level.SEVERE,
     } as logging.Entry));
-  });
+  });*/
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,39 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get(browser.baseUrl) as Promise<any>;
+    //return browser.get(browser.baseUrl) as Promise<any>;
+    return browser.get('/probleme');
   }
 
   getTitleText() {
-    return element(by.css('Inter-root h1')).getText() as Promise<string>;
+    //return element(by.css('Inter-root h5')).getText() as Promise<string>;
+    return element(by.css('Inter-root h5')).getText();
   }
+
+  setChampsValidesScenarioNominal() : void {
+    element(by.id('prenomId')).sendKeys('tonprenom');
+    element(by.id('nomId')).sendKeys('tonnom');
+    // Sélectionner le X élément dans la zone de liste déroulante
+    element(by.id('noTypeProblemeId')).all(by.tagName('option')).get(2).click();
+    // Cliquer sur le bouton radio voulu
+    element.all(by.id('notificationId')).get(0).click();
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');
+   } 
+
+   boutonSubmit() : ElementFinder { 
+    return element(by.buttonText('Sauvegarder'));
+  }  
+
+  setChampsValidesScenarioAlternatifParMessageTexte() : void {
+    element(by.id('prenomId')).sendKeys('tonprenom');
+    element(by.id('nomId')).sendKeys('tonnom');
+    // Sélectionner le X élément dans la zone de liste déroulante
+    element(by.id('noTypeProblemeId')).all(by.tagName('option')).get(2).click();
+    // Cliquer sur le bouton radio voulu
+    element.all(by.id('notificationId')).get(2).click();
+    element(by.id('telephoneId')).sendKeys('5141231234');
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');
+   } 
+   
 }
